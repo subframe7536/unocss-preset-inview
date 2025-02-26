425f0cce297b1f0ae2126591a47ff76c1e9b76b0
subframe7536 <1667077010@qq.com>
2025-02-26 08:53:12 +0800

    feat: add no-inview variant

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,13 @@ export interface PresetInViewOptions {
 
 export const presetInView = definePreset((options: PresetInViewOptions = {}) => {
   const { prefix = 'inview:' } = options
+  const prefixWithNo = `no-${prefix}`
   return {
     name: 'unocss-preset-inview',
     // Customize your variants here
     variants: [
       {
-        name: 'unocss-preset-inview',
+        name: 'unocss-variant-inview',
         match: (matcher) => {
           if (!matcher.startsWith(prefix)) {
             return matcher
@@ -23,6 +24,18 @@ export const presetInView = definePreset((options: PresetInViewOptions = {}) => 
           return {
             matcher: matcher.slice(prefix.length),
             selector: s => `${s}:not([no-inview])`,
+          }
+        },
+      },
+      {
+        name: 'unocss-variant-no-inview',
+        match: (matcher) => {
+          if (!matcher.startsWith(prefixWithNo)) {
+            return matcher
+          }
+          return {
+            matcher: matcher.slice(prefixWithNo.length),
+            selector: s => `${s}[no-inview]`,
           }
         },
       },
